9c2a4f53e79f7d0d7560142edd2cc8ac866f877c
Oscar Gaytan <oscar.gaytan@cloudsourceit.com>
2021-07-30 20:02:01 -0500

    Changed sidebar sizes

--- src/app/pages/components/sidebar/sidebar.component.ts
+++ src/app/pages/components/sidebar/sidebar.component.ts
@@ -12,6 +12,7 @@ export class SidebarComponent implements OnInit {
 
   public toggled:boolean = false;
   public items:Array<SidebarItem> = SidebarItems;
+  public icon:string = 'M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z';
 
   constructor(private sharedService:SharedService) { }
 
@@ -24,10 +25,12 @@ export class SidebarComponent implements OnInit {
     if (this.sharedService.expandedSidebar) {
       setTimeout(() => {
         this.toggled = true;
+        this.icon = 'M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5zM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5z';
         
       }, 1);
     } else{
       this.toggled = false;
+      this.icon = 'M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z';
     }
   }
 
